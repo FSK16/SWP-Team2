d643269f1b21a01ce2ce7876846e4c9f6ebe3f38
Felix Sourek <126675626+FSK16@users.noreply.github.com>
2023-12-04 13:34:22 +0100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php
+
+// Datenbankverbindungsparameter
+$servername = '185.51.8.82:3307';
+$username = 'u232847db2';
+$password = '';
+$dbname = 'u232847db2';
+// Erstellen einer Verbindung zur Datenbank
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Überprüfung der Verbindung
+if ($conn->connect_error) {
+ die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+// Setzen des Zeichensatzes für die Verbindung auf utf8
+
+$conn->set_charset("utf8");
+
+?>
